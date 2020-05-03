25fe402335a11e9e8e0713442c1f0a18904ee8cb
Владимир <black.ice.wizard@gmail.com>
2020-05-03 22:47:11 +0600

    Impact constructor

--- src/Object/FrontalImpact.php
+++ src/Object/FrontalImpact.php
@@ -2,7 +2,16 @@
 
 namespace App\Object;
 
+use App\Object\Rigid\Rigid;
+
 class FrontalImpact implements Phenomenon
 {
+    private Rigid $first;
+    private Rigid $second;
 
+    public function __construct(Rigid $first, Rigid $second)
+    {
+        $this->first = $first;
+        $this->second = $second;
+    }
 }
