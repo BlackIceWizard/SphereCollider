559c8236695bb1d89ac43c53264c8793192840bf
Владимир <black.ice.wizard@gmail.com>
2020-05-03 22:17:14 +0600

    Sphere constructor

--- src/Object/Rigid/Sphere.php
+++ src/Object/Rigid/Sphere.php
@@ -4,5 +4,12 @@ namespace App\Object\Rigid;
 
 class Sphere
 {
+    private float $weight;
+    private float $speed;
 
+    public function __construct(float $weight, float $speed)
+    {
+        $this->weight = $weight;
+        $this->speed = $speed;
+    }
 }
